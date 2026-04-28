2630809a72c288ab453a8971631e8a6a75f731df
Felipe Budinich <felipebudinich@Felipes-MacBook-Pro.local>
2026-04-28 15:20:43 -0400

    Add base href for Weltmeister media assets

--- test/weltmeister-html.test.mjs
+++ test/weltmeister-html.test.mjs
@@ -8,6 +8,7 @@ const retiredAssetPrefix = `lib${'-esm/'}`;
 test('tools/weltmeister.html boots the editor entirely from tools assets', async () => {
   const html = await fs.readFile(path.resolve('tools/weltmeister.html'), 'utf8');
 
+  assert.match(html, /<base href="\/"\/>/);
   assert.match(html, /href="\/tools\/weltmeister\/weltmeister\.css"/);
   assert.doesNotMatch(html, /src="lib\/weltmeister\/jquery-1\.7\.1\.min\.js"/);
   assert.doesNotMatch(html, /src="lib\/weltmeister\/jquery-ui-1\.8\.1\.custom\.min\.js"/);
