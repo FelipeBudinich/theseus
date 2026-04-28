9088ca7de2d0dac7be9dddd26360de6bb32122db
Felipe Budinich <felipebudinich@Felipes-MacBook-Pro.local>
2026-04-28 16:18:59 -0400

    Move texture atlas plugin under tools/bake

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import { fileURLToPath } from 'node:url';
 
 import { defineConfig } from 'vite';
 
-import { createTextureAtlasPlugin } from './tools/texture-atlas-plugin.mjs';
+import { createTextureAtlasPlugin } from './tools/bake/texture-atlas-plugin.mjs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
